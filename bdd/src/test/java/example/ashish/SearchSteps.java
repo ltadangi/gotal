15d5dff63d3688072d8e01234f4751fed0ead7a6
ltadangi <37258811+ltadangi@users.noreply.github.com>
2018-04-08 10:27:14 +0530

    Update SearchSteps.java
    updated set propert

--- bdd/src/test/java/example/ashish/SearchSteps.java
+++ bdd/src/test/java/example/ashish/SearchSteps.java
@@ -25,7 +25,7 @@ public class SearchSteps {
     @Before({"@requires_browser"})
     public void buildDriver() {
         //driver = new HtmlUnitDriver();
-        System.setProperty("webdriver.chrome.driver","\\C:\Leela\Softwares\chromedriver_win32");
+        System.setProperty("webdriver.chrome.driver","C:\Leela\Softwares\chromedriver_win32");
 
        
         driver = new ChromeDriver();
